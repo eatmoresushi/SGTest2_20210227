0677b3f2d9412d8a01f90a5cd30b5bc3ac83dab3
Luyuan He <luyuan@hey.com>
2021-02-27 12:59:19 +0000

    question1 set-up

--- question1/question1/spiders/aldi_spider.py
+++ question1/question1/spiders/aldi_spider.py
@@ -9,13 +9,42 @@ https://docs.scrapy.org/en/latest/intro/tutorial.html
 class AldiSpider(scrapy.Spider):
     name = "aldi"
 
-    def start_requests(self):
-        urls = ["https://www.aldi.com.au/"]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = ["https://www.aldi.com.au/"]
 
     def parse(self, response):
-        filename = "test.txt"
-        with open(filename, "w") as f:
-            f.write(response.xpath("//div[@id='footer-sitemap']").get())
-        self.log("saved")
+        # get all the submenu links of Groceries
+        submenu_links = response.xpath(
+            "//div[@id='footer-sitemap']/div[2]/div/div/ul/li/a/@href"
+        ).getall()
+        yield from response.follow_all(submenu_links, self.parse_submenu)
+
+    def parse_submenu(self, response):
+        product_tiles = response.xpath("//div[@class='tx-aldi-products']/div/a")
+        if len(product_tiles) > 0:
+            for product in product_tiles:
+                yield {
+                    "Product_title": product.xpath("./div/div/div[2]/div/text()")
+                    .get()
+                    .strip(),
+                    "Product_image": product.xpath("./div/div/div[1]/img/@src").get(),
+                    "Packsize": product.xpath(
+                        "./div/div/div[2]/div[2]/span[@class='box--amount']/text()"
+                    )
+                    .get()
+                    .strip(),
+                    "Price": product.xpath(
+                        "./div/div/div[2]/div[2]/span[@class='box--value']/text()"
+                    )
+                    .get()
+                    .strip()
+                    + product.xpath(
+                        "./div/div/div[2]/div[2]/span[@class='box--decimal']/text()"
+                    )
+                    .get()
+                    .strip(),
+                    "Price per unit": product.xpath(
+                        "./div/div/div[2]/div[2]/span[@class='box--baseprice']/text()"
+                    )
+                    .get()
+                    .strip(),
+                }
