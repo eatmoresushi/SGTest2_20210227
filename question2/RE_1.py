0e122e2098bf9d4be610545e663b332c7155930e
Luyuan He <luyuan@hey.com>
2021-02-28 06:57:13 +0000

    q2 first commit

---
+++ question2/RE_1.py
@@ -0,0 +1,4 @@
+import re
+
+product_count_text = "381 Products found"
+product_count_int = int(re.sub('[^0-9]', '', product_count_text))
---
+++ question2/RE_2.py
@@ -0,0 +1,6 @@
+import re
+
+generic_urls = ["https://www.genericdomain.com/abc/def/1290aodwb23-ghi.img", "https://www.genericdomain.com/abc/31287bdwakj-jkl.img", "https://www.genericdomain.com/19unioawd02-jkl.img"]
+
+for url in generic_urls:
+	special_sequence = re.search(r'/([a-zA-Z0-9]{11})-', url).group(1)
