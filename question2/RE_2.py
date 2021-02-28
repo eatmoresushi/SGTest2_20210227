84fc884d36d920c849c3dab3c126075089048759
Luyuan He <luyuan@hey.com>
2021-02-28 23:19:09 +0000

    modification on RE_2

--- question2/RE_2.py
+++ question2/RE_2.py
@@ -7,6 +7,9 @@ generic_urls = [
 ]
 
 for url in generic_urls:
-    # use [^/] means does not contain '/'
+    # if we are sure what we want does not contains '/'
+    # we can use [^/] which means does not contain '/'
     # so we are looking for everything between '/' and '-' while not contain '/'
     special_sequence = re.search(r"/([^/]*)-", url).group(1)
+    # Otherwise, if we are sure what we want has a fixed length of 11
+    # special_sequence = re.search(r"/(.{11})-", url).group(1)
